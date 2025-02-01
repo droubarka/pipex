6ffa7fe3552b765168cbb8a8e8dfcfdf40dc7906
Mohamed A. Oubarka <droubarka@gmail.com>
2025-02-01 05:06:53 +0100

    Add main() function to serve as the program's entry point.

---
+++ src/main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mait-oub <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/01 05:03:39 by mait-oub          #+#    #+#             */
+/*   Updated: 2025/02/01 05:03:40 by mait-oub         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	int	exit_status;
+
+	if (argc < 5)
+	{
+		errno = EINVAL;
+		perror(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	exit_status = pipex(argc - 1, argv + 1, envp);
+	return (exit_status);
+}
